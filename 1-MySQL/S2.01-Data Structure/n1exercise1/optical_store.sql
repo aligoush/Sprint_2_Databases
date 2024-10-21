22b006bf3160f4ed7aa7aa8ac3661065fc65798d
alinaltyn <alieneguzh@gmail.com>
2024-10-21 17:45:10 +0200

    add brand entity

--- 1-MySQL/S2.01-Data Structure/n1exercise1/optical_store.sql
+++ 1-MySQL/S2.01-Data Structure/n1exercise1/optical_store.sql
@@ -1,108 +1,171 @@
 -- MySQL Workbench Synchronization
--- Generated: 2024-10-11 20:30
+-- Generated: 2024-10-21 17:44
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
--- Author: alina
+-- Author: alien
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-ALTER SCHEMA `optical_store`  DEFAULT COLLATE utf8mb4_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `optical_store` DEFAULT CHARACTER SET utf8 ;
 
-ALTER TABLE `optical_store`.`suppliers` 
-DROP FOREIGN KEY `fk_suppliers_address1`;
+CREATE TABLE IF NOT EXISTS `optical_store`.`address` (
+  `address_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `country` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `city` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `postal_code` VARCHAR(5) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `street` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `street_number` INT(11) NOT NULL,
+  `floor` INT(11) NOT NULL,
+  `door` INT(11) NOT NULL,
+  PRIMARY KEY (`address_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`glasses` 
-DROP FOREIGN KEY `fk_glasses_suppliers1`;
+CREATE TABLE IF NOT EXISTS `optical_store`.`customers` (
+  `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customer_name` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `customer_phone_number` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `customer_email` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `registration_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `referred_by` INT(11) NULL DEFAULT NULL,
+  `address_id` INT(11) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE INDEX `customer_email_UNIQUE` (`customer_email` ASC) VISIBLE,
+  UNIQUE INDEX `customer_phone_number_UNIQUE` (`customer_phone_number` ASC) VISIBLE,
+  UNIQUE INDEX `referred_by_UNIQUE` (`referred_by` ASC) VISIBLE,
+  INDEX `fk_customers_customers1_idx` (`referred_by` ASC) VISIBLE,
+  INDEX `fk_customers_address1` (`address_id` ASC) VISIBLE,
+  CONSTRAINT `fk_customers_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `optical_store`.`address` (`address_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_customers_customers1`
+    FOREIGN KEY (`referred_by`)
+    REFERENCES `optical_store`.`customers` (`customer_id`)
+    ON DELETE SET NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`sales` 
-DROP FOREIGN KEY `fk_sales_customers1`,
-DROP FOREIGN KEY `fk_sales_employees1`,
-DROP FOREIGN KEY `fk_sales_glasses1`;
+CREATE TABLE IF NOT EXISTS `optical_store`.`employees` (
+  `employees_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `employee_phone_number` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  PRIMARY KEY (`employees_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`suppliers` 
-COLLATE = utf8mb4_general_ci ,
-CHANGE COLUMN `supplier_name` `supplier_name` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `supplier_phone_number` `supplier_phone_number` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `supplier_fax` `supplier_fax` VARCHAR(45) NULL DEFAULT NULL ,
-CHANGE COLUMN `supplier_nif` `supplier_nif` VARCHAR(9) NOT NULL ,
-CHANGE COLUMN `address_id` `address_id` INT(11) NOT NULL ;
+CREATE TABLE IF NOT EXISTS `optical_store`.`glasses` (
+  `glasses_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `prescription_lens_left` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `prescription_lens_right` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `frame_type` ENUM('floating', 'plastic', 'metallic') COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `frame_color` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `lens_color_left` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `lens_color_right` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `brand_id` INT(11) NOT NULL,
+  `supplier_id` INT(11) NOT NULL,
+  PRIMARY KEY (`glasses_id`),
+  INDEX `fk_glasses_suppliers1_idx1` (`supplier_id` ASC) VISIBLE,
+  INDEX `fk_glasses_brands1_idx` (`brand_id` ASC) VISIBLE,
+  CONSTRAINT `fk_glasses_suppliers1`
+    FOREIGN KEY (`supplier_id`)
+    REFERENCES `optical_store`.`suppliers` (`supplier_id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_glasses_brands1`
+    FOREIGN KEY (`brand_id`)
+    REFERENCES `optical_store`.`brands` (`brand_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`glasses` 
-COLLATE = utf8mb4_general_ci ,
-CHANGE COLUMN `glasses_brand` `glasses_brand` VARCHAR(100) NOT NULL ,
-CHANGE COLUMN `prescription_lens_left` `prescription_lens_left` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `prescription_lens_right` `prescription_lens_right` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `frame_type` `frame_type` ENUM("floating", "plastic", "metallic") NOT NULL ,
-CHANGE COLUMN `frame_color` `frame_color` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `lens_color_left` `lens_color_left` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `lens_color_right` `lens_color_right` VARCHAR(45) NOT NULL ;
+CREATE TABLE IF NOT EXISTS `optical_store`.`sales` (
+  `sale_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` INT(11) NOT NULL,
+  `employee_id` INT(11) NOT NULL,
+  `glasses_id` INT(11) NOT NULL,
+  `sale_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sale_id`),
+  INDEX `fk_sales_customers1_idx` (`customer_id` ASC) VISIBLE,
+  INDEX `fk_sales_employees1_idx` (`employee_id` ASC) VISIBLE,
+  INDEX `fk_sales_glasses1_idx` (`glasses_id` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`customers` 
-COLLATE = utf8mb4_general_ci ,
-CHANGE COLUMN `customer_name` `customer_name` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `customer_phone_number` `customer_phone_number` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `customer_email` `customer_email` VARCHAR(45) NOT NULL ,
-DROP INDEX `fk_customers_address1` ;
-;
+CREATE TABLE IF NOT EXISTS `optical_store`.`suppliers` (
+  `supplier_id` INT(11) NOT NULL,
+  `supplier_name` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `supplier_phone_number` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `supplier_fax` VARCHAR(45) COLLATE 'utf8mb3_estonian_ci' NULL DEFAULT NULL,
+  `supplier_nif` VARCHAR(9) COLLATE 'utf8mb3_estonian_ci' NOT NULL,
+  `address_id` INT(11) NOT NULL,
+  PRIMARY KEY (`supplier_id`),
+  UNIQUE INDEX `supplier_nif_UNIQUE` (`supplier_nif` ASC) VISIBLE,
+  UNIQUE INDEX `supplier_phone_number_UNIQUE` (`supplier_phone_number` ASC) VISIBLE,
+  INDEX `fk_suppliers_address1_idx1` (`address_id` ASC) VISIBLE,
+  CONSTRAINT `fk_suppliers_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `optical_store`.`address` (`address_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`sales` 
-COLLATE = utf8mb4_general_ci ;
+CREATE TABLE IF NOT EXISTS `optical_store`.`employees` (
+  `employee_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `phone_number` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`employee_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`employees` 
-COLLATE = utf8_estonian_ci ,
-CHANGE COLUMN `employees_id` `employee_id` INT(11) NOT NULL AUTO_INCREMENT ,
-CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL ,
-CHANGE COLUMN `employee_phone_number` `employee_phone_number` VARCHAR(45) NOT NULL ;
+CREATE TABLE IF NOT EXISTS `optical_store`.`sales` (
+  `sale_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` INT(11) NOT NULL,
+  `glasses_id` INT(11) NOT NULL,
+  `employee_id` INT(11) NOT NULL,
+  `sale_date` DATETIME NOT NULL,
+  PRIMARY KEY (`sale_id`),
+  INDEX `fk_customers_has_glasses_glasses1_idx` (`glasses_id` ASC) VISIBLE,
+  INDEX `fk_customers_has_glasses_customers1_idx` (`customer_id` ASC) VISIBLE,
+  INDEX `fk_sales_employees1_idx` (`employee_id` ASC) VISIBLE,
+  CONSTRAINT `fk_customers_has_glasses_customers1`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `optical_store`.`customers` (`customer_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_customers_has_glasses_glasses1`
+    FOREIGN KEY (`glasses_id`)
+    REFERENCES `optical_store`.`glasses` (`glasses_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_sales_employees1`
+    FOREIGN KEY (`employee_id`)
+    REFERENCES `optical_store`.`employees` (`employee_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `optical_store`.`address` 
-COLLATE = utf8mb4_general_ci ,
-CHANGE COLUMN `country` `country` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `city` `city` VARCHAR(45) NOT NULL ,
-CHANGE COLUMN `postal_code` `postal_code` VARCHAR(5) NOT NULL ,
-CHANGE COLUMN `street` `street` VARCHAR(45) NOT NULL ;
-
-ALTER TABLE `optical_store`.`suppliers` 
-ADD CONSTRAINT `fk_suppliers_address1`
-  FOREIGN KEY (`address_id`)
-  REFERENCES `optical_store`.`address` (`address_id`)
-  ON DELETE RESTRICT
-  ON UPDATE CASCADE;
-
-ALTER TABLE `optical_store`.`glasses` 
-ADD CONSTRAINT `fk_glasses_suppliers1`
-  FOREIGN KEY (`supplier_id`)
-  REFERENCES `optical_store`.`suppliers` (`supplier_id`)
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE;
-
-ALTER TABLE `optical_store`.`customers` 
-DROP FOREIGN KEY `fk_customers_customers1`;
-
-ALTER TABLE `optical_store`.`customers` ADD CONSTRAINT `fk_customers_customers1`
-  FOREIGN KEY (`referred_by`)
-  REFERENCES `optical_store`.`customers` (`customer_id`)
-  ON DELETE SET NULL
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `optical_store`.`sales` 
-ADD CONSTRAINT `fk_sales_customers1`
-  FOREIGN KEY (`customer_id`)
-  REFERENCES `optical_store`.`customers` (`customer_id`)
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE,
-ADD CONSTRAINT `fk_sales_employees1`
-  FOREIGN KEY (`employee_id`)
-  REFERENCES `optical_store`.`employees` (`employee_id`)
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE,
-ADD CONSTRAINT `fk_sales_glasses1`
-  FOREIGN KEY (`glasses_id`)
-  REFERENCES `optical_store`.`glasses` (`glasses_id`)
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `optical_store`.`brands` (
+  `brand_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `brand_name` VARCHAR(100) NOT NULL,
+  `supplier_id` INT(11) NOT NULL,
+  PRIMARY KEY (`brand_id`),
+  INDEX `fk_brands_suppliers1_idx` (`supplier_id` ASC) VISIBLE,
+  CONSTRAINT `fk_brands_suppliers1`
+    FOREIGN KEY (`supplier_id`)
+    REFERENCES `optical_store`.`suppliers` (`supplier_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
